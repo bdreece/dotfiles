988fd78a89f2015791ab66c1968db545244eb8e7
Brian Reece <bdreece@mtu.edu>
2023-06-15 22:53:00 -0400

    Removed unused dotfiles

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#9bc1dd", "#0d1215" },
-	[SchemeSel] = { "#9bc1dd", "#575966" },
-	[SchemeOut] = { "#9bc1dd", "#2E5399" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#0d1215";
-static const char* selfgcolor   = "#9bc1dd";
-static const char* normbgcolor  = "#A45A4E";
-static const char* normfgcolor  = "#9bc1dd";
-static const char* urgbgcolor   = "#575966";
-static const char* urgfgcolor   = "#9bc1dd";
